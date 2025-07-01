e92b916a2e037c903f4124ff6296402337eb6dda
onier9702 <onier0217@gmail.com>
2025-07-01 13:28:29 -0600

    [bugfix] sales overview method fixed

--- src/modules/overview/overview.module.ts
+++ src/modules/overview/overview.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '../auth/auth.module';
 
 import { Quote } from '../quote/entities/quote.entity';
+import { PurchaseOrder } from '../purchase-order/entities/purchase-order.entity';
 import { Invoice } from '../invoice/entities/invoice.entity';
 
 import { OverviewController } from './overview.controller';
@@ -12,6 +13,9 @@ import { OverviewService } from './overview.service';
 @Module({
   controllers: [OverviewController],
   providers: [OverviewService],
-  imports: [TypeOrmModule.forFeature([Quote, Invoice]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([Quote, Invoice, PurchaseOrder]),
+    AuthModule,
+  ],
 })
 export class OverviewModule {}
--- src/modules/overview/overview.service.ts
+++ src/modules/overview/overview.service.ts
@@ -4,7 +4,14 @@ import { Repository } from 'typeorm';
 
 import { Quote } from '../quote/entities/quote.entity';
 import { Invoice } from '../invoice/entities/invoice.entity';
-import { NameEntities } from 'src/enums';
+import { PurchaseOrder } from '../purchase-order/entities/purchase-order.entity';
+
+import {
+  NameEntities,
+  StatusInvoice,
+  StatusPurchaseOrder,
+  StatusQuote,
+} from 'src/enums';
 
 @Injectable()
 export class OverviewService {
@@ -13,6 +20,8 @@ export class OverviewService {
     private readonly quoteRepository: Repository<Quote>,
     @InjectRepository(Invoice)
     private readonly invoiceRepository: Repository<Invoice>,
+    @InjectRepository(PurchaseOrder)
+    private readonly purchaseOrderRepository: Repository<PurchaseOrder>,
   ) {}
 
   async getSalesOverview(): Promise<{
@@ -25,13 +34,32 @@ export class OverviewService {
       Record<string, number>
     >;
   }> {
+    const allowedQuoteStatuses = [
+      StatusQuote.DRAFT,
+      StatusQuote.SENT,
+      StatusQuote.ACCEPTED,
+    ];
+
+    const allowedInvoiceStatuses = [StatusInvoice.DRAFT];
+
     const [quoteStats, invoiceStats] = await Promise.all([
       this.quoteRepository
         .createQueryBuilder('quote')
         .select('quote.status', 'status')
         .addSelect('COUNT(*)', 'count')
         .addSelect('SUM(quote.total)', 'total')
+        .where('quote.status IN (:...allowedStatuses)', {
+          allowedStatuses: allowedQuoteStatuses,
+        })
         .groupBy('quote.status')
+        .orderBy(
+          `CASE
+          WHEN quote.status = 'borrador' THEN 1
+          WHEN quote.status = 'enviada' THEN 2
+          WHEN quote.status = 'aceptada' THEN 3
+          ELSE 4
+          END`,
+        )
         .getRawMany(),
 
       this.invoiceRepository
@@ -39,7 +67,17 @@ export class OverviewService {
         .select('invoice.status', 'status')
         .addSelect('COUNT(*)', 'count')
         .addSelect('SUM(invoice.total)', 'total')
+        .where('invoice.status IN (:...allowedStatuses)', {
+          allowedStatuses: allowedInvoiceStatuses,
+        })
         .groupBy('invoice.status')
+        .orderBy(
+          `CASE
+          WHEN invoice.status = 'borrador' THEN 1
+          WHEN invoice.status = 'enviada' THEN 2
+          ELSE 3
+          END`,
+        )
         .getRawMany(),
     ]);
 
@@ -66,13 +104,32 @@ export class OverviewService {
     statusCounts: Record<NameEntities.PURCHASE_ORDER, Record<string, number>>;
     totalsByStatus: Record<NameEntities.PURCHASE_ORDER, Record<string, number>>;
   }> {
+    const allowedPurchaseOrderStatuses = [
+      StatusPurchaseOrder.DRAFT,
+      StatusPurchaseOrder.SENT,
+      StatusPurchaseOrder.AWAITING_APPROVAL,
+      StatusPurchaseOrder.APPROVED,
+    ];
+
     const [purchaseOrderStats] = await Promise.all([
-      this.quoteRepository
+      this.purchaseOrderRepository
         .createQueryBuilder('purchase_order')
         .select('purchase_order.status', 'status')
         .addSelect('COUNT(*)', 'count')
         .addSelect('SUM(purchase_order.total)', 'total')
+        .where('purchase_order.status IN (:...allowedStatuses)', {
+          allowedStatuses: allowedPurchaseOrderStatuses,
+        })
         .groupBy('purchase_order.status')
+        .orderBy(
+          `CASE
+          WHEN purchase_order.status = 'borrador' THEN 1
+          WHEN purchase_order.status = 'enviada' THEN 2
+          WHEN purchase_order.status = 'esperando_aprobación' THEN 3
+          WHEN purchase_order.status = 'aprobada' THEN 4
+          ELSE 5
+          END`,
+        )
         .getRawMany(),
     ]);
 
@@ -89,4 +146,32 @@ export class OverviewService {
       totalsByStatus,
     };
   }
+
+  // private readonly customStatusOrder = [
+  //   'draft',
+  //   'sent',
+  //   'accepted',
+  //   'declined',
+  // ];
+
+  // private sortByCustomStatusOrder<T extends Record<string, number>>(
+  //   data: T,
+  // ): T {
+  //   const sorted: Record<string, number> = {};
+
+  //   for (const status of this.customStatusOrder) {
+  //     if (status in data) {
+  //       sorted[status] = data[status];
+  //     }
+  //   }
+
+  //   // Add any remaining statuses that were not in the predefined order
+  //   for (const key of Object.keys(data)) {
+  //     if (!(key in sorted)) {
+  //       sorted[key] = data[key];
+  //     }
+  //   }
+
+  //   return sorted as T;
+  // }
 }
